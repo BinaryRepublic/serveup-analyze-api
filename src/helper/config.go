0c89ff8446e7e5ced5b8c55aaf65c32cb233c990
Tobias Trinks <tobias.trinks@code.berlin>
2018-03-10 12:03:39 +0100

    updated config for devops environments

--- src/helper/config.go
+++ src/helper/config.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"github.com/BurntSushi/toml"
 	"path/filepath"
+	"os"
 )
 
 type orderApi struct {
@@ -14,16 +15,36 @@ type soundFiles struct {
 	Path		string
 	Type		string
 }
-type config struct {
+type environment struct {
 	OrderApi	orderApi
 	SoundFiles	soundFiles
 }
+type config struct {
+	Dev 	environment
+	Prod 	environment
+	Local0 	environment
+	Local1 	environment
+	Local2 	environment
+}
 
-func ReadConfig() config {
+func ReadConfig() environment {
 	var config config
 	absPath, _ := filepath.Abs("config.toml")
 	if _, err := toml.DecodeFile(absPath, &config); err != nil {
 		log.Fatal(err)
 	}
-	return config
+	var configEnv environment
+
+	if os.Getenv("DEV") != "" {
+		configEnv = config.Dev
+	} else if os.Getenv("PROD") != "" {
+		configEnv = config.Prod
+	} else if os.Getenv("LOCAL0") != "" {
+		configEnv = config.Local0
+	} else if os.Getenv("LOCAL1") != "" {
+		configEnv = config.Local1
+	} else if os.Getenv("LOCAL2") != "" {
+		configEnv = config.Local2
+	}
+	return configEnv
 }
